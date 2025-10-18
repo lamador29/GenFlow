99131758a3d9dac130a5bbbdb846e631c649190f
Laura Amador <85769614+lamador29@users.noreply.github.com>
2025-10-18 00:19:02 -0600

    Changes in structure of the code and genflow -> snakemake

--- workflow/pytools/align_mafft.py
+++
@@ -1,9 +0,0 @@
-import pathlib, subprocess
-def main(in_fasta, out_aln, threads=2):
-    out = pathlib.Path(out_aln); out.parent.mkdir(parents=True, exist_ok=True)
-    cmd = ["mafft","--retree","1","--thread",str(threads),"--maxiterate","0",in_fasta]
-    with open(out,"w") as out_fh:
-        subprocess.check_call(cmd, stdout=out_fh)
-if __name__ == "__main__":
-    thr = int(snakemake.config.get("threads",2))
-    main(snakemake.input[0], snakemake.output[0], thr)
--- workflow/pytools/tree_fasttree.py
+++
@@ -1,7 +0,0 @@
-import subprocess, pathlib
-def main(in_alignment, out_tree):
-    out = pathlib.Path(out_tree); out.parent.mkdir(parents=True, exist_ok=True)
-    with open(out,"w") as fh:
-        subprocess.check_call(["FastTree","-nt", in_alignment], stdout=fh)
-if __name__ == "__main__":
-    main(snakemake.input[0], snakemake.output[0])
